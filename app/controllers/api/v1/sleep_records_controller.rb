9a5fd36872b766f4dca2649b925b1c54a3b1bd07
AJ Villalobos <hello@ajvillalobos.com>
2023-04-10 19:40:22 +0800

    Enable creation of SleepRecord via /clock_in and /clock_out endpoints

--- app/controllers/api/v1/sleep_records_controller.rb
+++ app/controllers/api/v1/sleep_records_controller.rb
@@ -1,9 +1,21 @@
 class Api::V1::SleepRecordsController < ApplicationController
   def create
-    render json: {message: "You are clocked in"}, status: :created
+    sleep_record = Current.user.sleep_records.new(clock_in: Time.current)
+
+    if sleep_record.save
+      render json: {message: "You are clocked in", data: sleep_record},
+        status: :ok
+    else
+      render json: {error: sleep_record.errors}, status: :unprocessable_entity
+    end
   end
 
   def update
-    render json: {message: "You are clocked out"}, status: :ok
+    sleep_record = Current.user.sleep_records.where(clock_out: nil).last
+    if sleep_record.update(clock_out: Time.current)
+      render json: {message: "You are clocked out"}, status: :ok
+    else
+      render json: {error: sleep_record.errors}, status: :unprocessable_entity
+    end
   end
 end
--- test/controllers/sleep_records_controller_test.rb
+++ test/controllers/sleep_records_controller_test.rb
@@ -1,13 +1,48 @@
 require "test_helper"
 
-class SleepRecordsControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get sleep_records_create_url
-    assert_response :success
+class Api::V1::SleepRecordsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:one)
+    @sleep_record = sleep_records(:one)
+    @sleep_record_clock_in_only = sleep_records(:clock_in_only)
   end
 
-  test "should get update" do
-    get sleep_records_update_url
+  def authenticate_user
+    {"Authorization" => @user.auth_token}
+  end
+
+  test "should create sleep record" do
+    assert_difference("SleepRecord.count") do
+      post clock_in_url, headers: authenticate_user
+    end
+
     assert_response :success
+    json_response = JSON.parse(@response.body)
+    assert_equal "You are clocked in", json_response["message"]
+    assert_not_nil json_response["data"]
+  end
+
+  test "should not create sleep record without authentication" do
+    assert_no_difference("SleepRecord.count") do
+      post clock_in_url
+    end
+
+    assert_response :unauthorized
+  end
+
+  test "should update sleep record" do
+    put clock_out_url, headers: authenticate_user
+
+    assert_response :success
+    json_response = JSON.parse(@response.body)
+    assert_equal "You are clocked out", json_response["message"]
+    @sleep_record_clock_in_only.reload
+    assert_not_nil @sleep_record_clock_in_only.clock_out
+  end
+
+  test "should not update sleep record without authentication" do
+    put clock_out_url
+
+    assert_response :unauthorized
   end
 end
