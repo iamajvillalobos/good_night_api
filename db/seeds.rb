6cf24e3e17962c10a1d4612343c5873b6fe8701e
AJ Villalobos <hello@ajvillalobos.com>
2023-04-12 13:02:48 +0800

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,12 @@
-User.create!(name: "AJ")
-User.create!(name: "Bobby")
-User.create!(name: "Cindy")
+user1 = User.create!(name: "AJ")
+user2 = User.create!(name: "Bobby")
+user3 = User.create!(name: "Cindy")
+
+SleepRecord.create!(user_id: user1.id, clock_in: 1.day.ago, clock_out: 1.day.ago + 8.hours)
+SleepRecord.create!(user_id: user2.id, clock_in: 1.day.ago, clock_out: 1.day.ago + 8.hours)
+SleepRecord.create!(user_id: user3.id, clock_in: 1.day.ago, clock_out: 1.day.ago + 8.hours)
+SleepRecord.create!(user_id: user2.id, clock_in: 2.days.ago, clock_out: 2.days.ago + 8.hours)
+SleepRecord.create!(user_id: user3.id, clock_in: 2.days.ago, clock_out: 2.days.ago + 8.hours)
+
+Follow.create!(follower_id: user1.id, followee_id: user2.id)
+Follow.create!(follower_id: user1.id, followee_id: user3.id)
